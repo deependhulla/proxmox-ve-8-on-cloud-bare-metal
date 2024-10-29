00d132d5e03cb04d802cb167f46b243a34218a12
Deepen Dhulla <deepen@deependhulla.com>
2024-10-29 13:02:19 +0530

    update 2024-10-29

--- 04-proxmox-development-repo-setup.sh
+++ 04-proxmox-development-repo-setup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+## Please use Enterprise subscription for Stable packages.
 
-echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-enterprise.list
-echo "deb http://download.proxmox.com/debian/ceph-quincy bookworm no-subscription" > /etc/apt/sources.list.d/ceph.list
-wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg 
+
+echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+
+wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg 
+
 
 apt-get update
 apt-get -y upgrade
