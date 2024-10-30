c679e51734ae0e9665d9d8cdbe91d86399a75299
Deepen Dhulla <deepen@deependhulla.com>
2024-10-30 13:14:02 +0530

    update 2024-10-30

--- 01-locales-setup-and-check-hostname-in-host-files.sh
+++ 01-locales-setup-and-check-hostname-in-host-files.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 
 
+
+apt-get install -y whiptail
+
+whiptail --title "Check Your HostName and DateTime" --infobox "Please make sure your System Date Time is OK in Sync\n `date` \n\n Also Make sure hostname is set proper \n Host: `hostname -f`" 12 58
+
 # for Debian 12
 ## set to India IST timezone -- You can dissable it if needed
 export LANG=C
@@ -15,3 +20,7 @@ echo "LANG=en_US.UTF-8" > /etc/environment
 echo "LC_ALL=en_US.UTF-8" >> /etc/environment
 update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8
 
+echo "You see hostname"
+hostname -f 
+echo "you should see single ip only for (hostname -i) ...else check /etc/hosts and add or update hostname for ip"
+hostname -i
