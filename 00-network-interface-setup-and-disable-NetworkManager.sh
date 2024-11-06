1d6eb9a05ad9cce89b070f8def44ccefbd72dec4
Deepen Dhulla <deepen@deependhulla.com>
2024-11-06 12:42:41 +0530

    update 2024-11-06

--- 00-network-interface-setup-and-disable-NetworkManager.sh
+++ 00-network-interface-setup-and-disable-NetworkManager.sh
@@ -8,4 +8,4 @@ echo "------------"
 echo "MAKE SURE YOU HAVE SET ROOT PASSWORD AND ABLE OT LOGIN VIA AWS SERIAL CONSOLE FOR TROUBLESHOOT IF NETWORK GOES AWAY"
 
 apt -y install ifupdown bridge-utils net-tools ethtool
-
+apt -y install ifupdown2
